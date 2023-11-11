55ba223512587dee98b57db33bc4e072237edec7
David Carr <dave@dcblog.dev>
2023-11-11 15:47:21 +0000

    updated tests

--- src/Tags.php
+++ src/Tags.php
@@ -4,14 +4,63 @@ namespace Dcblogdev\Tags;
 
 class Tags
 {
-    public static function get(string $string): string
+    public function get(string $content): string
     {
         //Current year
-        $string = str_replace('[year]', date('Y'), $string);
+        $content = str_replace('[year]', date('Y'), $content);
 
         //Name of website
-        $string = str_replace('[appName]', config('app.name'), $string);
+        $content = str_replace('[appName]', config('app.name'), $content);
 
-        return $string;
+        //youtube embeds
+        $content = preg_replace_callback("(\[youtube (.*?)])is", function ($matches) {
+            $params = $this->clean($matches);
+
+            //if key exits use it
+            $video  = $params['//www.youtube.com/watch?v'];
+            $width  = ($params['width'] ?? '560');
+            $height = ($params['height'] ?? '360');
+
+            return "<iframe width='$width' height='$height' src='//www.youtube.com/embed/$video' frameborder='0' allowfullscreen></iframe>";
+        }, $content);
+
+
+        return $content;
+    }
+
+    private function clean(array|string $data): array
+    {
+        // Check if $data is an array and extract the string to be processed.
+        $stringToProcess = is_array($data) && isset($data[1]) ? $data[1] : $data;
+
+        // Ensure that the stringToProcess is actually a string.
+        if (!is_string($stringToProcess)) {
+            // Handle error or return an empty array
+            return [];
+        }
+
+        $parts = explode(' ', $stringToProcess);
+        $params = [];
+
+        foreach ($parts as $part) {
+            if (!empty($part)) {
+                if (str_contains($part, '=')) {
+                    [$name, $value] = explode('=', $part, 2);
+                    $value = $this->removeCharsFromString($value);
+                    $name = $this->removeCharsFromString($name);
+                    $params[$name] = $value;
+                } else {
+                    $params[] = $this->removeCharsFromString($part);
+                }
+            }
+        }
+
+        return $params;
+    }
+
+    private function removeCharsFromString(string $value): string
+    {
+        $search = ['http:', 'https:', '&quot;', '&rdquo;', '&rsquo;', '&nbsp;'];
+        return str_replace($search, '', $value);
     }
 }
--- tests/TagsTest.php
+++ tests/TagsTest.php
@@ -14,3 +14,24 @@ test('[appName] sets the name of the app', function () {
 
     expect($response)->toBe('Laravel');
 });
+
+test('can embed youtube videos', function () {
+    $response = Tags::get('[youtube https://www.youtube.com/watch?v=cAzwfFPmghY]');
+
+    expect($response)
+        ->toBe("<iframe width='560' height='360' src='//www.youtube.com/embed/cAzwfFPmghY' frameborder='0' allowfullscreen></iframe>");
+});
+
+test('can embed youtube videos with width and height', function () {
+    $response = Tags::get('[youtube https://www.youtube.com/watch?v=cAzwfFPmghY width=100 height=100]');
+
+    expect($response)
+        ->toBe("<iframe width='100' height='100' src='//www.youtube.com/embed/cAzwfFPmghY' frameborder='0' allowfullscreen></iframe>");
+});
+
+test('can embed youtube videos with height', function () {
+    $response = Tags::get('[youtube https://www.youtube.com/watch?v=cAzwfFPmghY height=100]');
+
+    expect($response)
+        ->toBe("<iframe width='560' height='100' src='//www.youtube.com/embed/cAzwfFPmghY' frameborder='0' allowfullscreen></iframe>");
+});
